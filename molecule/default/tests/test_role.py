7a186088a463ec2cfbcc9bd35e5626327c078e3b
Nicolas Boutet <amd3002@gmail.com>
2023-04-28 12:52:04 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,22 +5,29 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('nodejs'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("nodejs"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('path,user,group', [
-  ('/usr/bin/node', 'root', 'root'),
-  ('/usr/bin/ncu', 'root', 'root'),
-  ('/usr/bin/yarn', 'root', 'root'),
-])
+@pytest.mark.parametrize(
+    "path,user,group",
+    [
+        ("/usr/bin/node", "root", "root"),
+        ("/usr/bin/ncu", "root", "root"),
+        ("/usr/bin/yarn", "root", "root"),
+    ],
+)
 def test_binaries_are_installed(host, path, user, group):
     binary = host.file(path)
     assert binary.exists
